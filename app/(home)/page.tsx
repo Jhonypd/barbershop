26ef28c3173df8bdfcc4ec04610a335166d05e3c
Jhony Pereira <jhony-16@live.com>
2024-01-30 12:19:54 -0300

    feat: add  welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,19 @@
 /** @format */
 
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Jhony!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
     </div>
   );
 }
